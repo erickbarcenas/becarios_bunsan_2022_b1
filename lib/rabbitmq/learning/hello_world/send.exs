c5e4a4f4a7daab172583a59e217f0edf90db9e61
Erick Barcenas <barcenas.dev@gmail.com>
2022-05-12 17:41:00 -0500

    building architecture RabbitMQ

--- lib/rabbitmq/learning/hello_world/send.exs
+++ lib/rabbitmq/learning/hello_world/send.exs
@@ -1,6 +1,15 @@
+# iex> options = [host: "localhost", port: 5672, virtual_host: "/", username: "guest", password: "guest"]
+# iex> AMQP.Connection.open(options)
+# {:ok, %AMQP.Connection{}}
+# 
+# iex> AMQP.Connection.open("amqp://guest:guest@localhost")
+# {:ok, %AMQP.Connection{}}
+
+
 {:ok, connection} = AMQP.Connection.open()
-{:ok, channel} = AMQP.Channel.open(connection)
-AMQP.Queue.declare(channel, "hello")
-AMQP.Basic.publish(channel, "", "hello", "Hello World!")
+{:ok, channel_a} = AMQP.Channel.open(connection)
+
+AMQP.Queue.declare(channel_a, "hello")
+AMQP.Basic.publish(channel_a, "", "hello", "Hello World!")
 IO.puts(" [x] Sent 'Hello World!'")
 AMQP.Connection.close(connection)
