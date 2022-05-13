43d16140f83ad0dba308f1382d7f997411305ca9
Erick Barcenas <barcenas.dev@gmail.com>
2022-05-13 11:26:23 -0500

    organizing modules into separate files

---
+++ lib/rabbitmq/case_study/consumer.ex
@@ -0,0 +1,40 @@
+defmodule RabbitMQ.Consumer do
+    use AMQP
+  
+    @doc """
+    Creates a process that listens for messages on the given queue.
+    When a message arrives, it writes to the log the pid, queue_name and msg.
+    Example:
+      iex> {:ok, pid} = Consumer.start("orders")
+    """
+    def start(queue_name) do
+      connection = Connection.open() |> elem(1)
+      channel = Channel.open(connection) |> elem(1)
+      spawn(fn -> listen_messages(queue_name, connection, channel) end)
+    end
+  
+    defp listen_messages(queue_name, connection, channel) do
+      Basic.consume(channel, queue_name, nil, no_ack: true)
+  
+      receive do
+        {:basic_deliver, payload, _meta} ->
+          IO.puts("#{inspect(self())} just got #{payload} from #{queue_name}")
+          Connection.close(connection)
+          listen_messages(queue_name, connection, channel)
+  
+        {:stop} ->
+          Connection.close(connection)
+          :ok
+      end
+    end
+  
+    @doc """
+    Stops the given consumer.
+    Example:
+      iex> Consumer.stop("orders")
+    """
+    def stop(pid) do
+      send(pid, {:stop})
+    end
+  end
+  
